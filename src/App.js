f5cd5f0eae2e7b51701e3da4fdfa0832d4fe4a56
adityakgp <adityadas.c24@gmail.com>
2021-10-19 19:58:21 +0530

    class based functions

--- src/App.js
+++ src/App.js
@@ -14,8 +14,9 @@ import {
 
 
 export default class App extends Component {
+  apiKey= process.env.REACT_APP_NEWS_API
   pageSize=12;
-  country='us';
+  country='in';
   state={
       progress :0
   }
@@ -36,13 +37,13 @@ export default class App extends Component {
         // onLoaderFinished={() => setProgress(0)}
       />
         <Switch>
-          <Route exact path="/"><News setProgress={this.setProgress} key='general' pageSize={this.pageSize} country={this.country} category={'general'}/></Route>
-          <Route exact path="/business"><News setProgress={this.setProgress} key='business' pageSize={this.pageSize} country={this.country} category={'business'}/></Route>
-          <Route exact path="/entertainment"><News setProgress={this.setProgress} key='entertainment' pageSize={this.pageSize} country={this.country} category={'entertainment'}/></Route>
-          <Route exact path="/health"><News setProgress={this.setProgress} key='health' pageSize={this.pageSize} country={this.country} category={'health'}/></Route>
-          <Route exact path="/science"><News setProgress={this.setProgress} key='science' pageSize={this.pageSize} country={this.country} category={'science'}/></Route>
-          <Route exact path="/sports"><News setProgress={this.setProgress} key='sports' pageSize={this.pageSize} country={this.country} category={'sports'}/></Route>
-          <Route exact path="/technology"><News setProgress={this.setProgress} key='technology' pageSize={this.pageSize} country={this.country} category={'technology'}/></Route>
+          <Route exact path="/"><News setProgress={this.setProgress} apiKey={this.apiKey} key='general' pageSize={this.pageSize} country={this.country} category={'general'}/></Route>
+          <Route exact path="/business"><News setProgress={this.setProgress} apiKey={this.apiKey} key='business' pageSize={this.pageSize} country={this.country} category={'business'}/></Route>
+          <Route exact path="/entertainment"><News setProgress={this.setProgress} apiKey={this.apiKey} key='entertainment' pageSize={this.pageSize} country={this.country} category={'entertainment'}/></Route>
+          <Route exact path="/health"><News setProgress={this.setProgress} apiKey={this.apiKey} key='health' pageSize={this.pageSize} country={this.country} category={'health'}/></Route>
+          <Route exact path="/science"><News setProgress={this.setProgress} apiKey={this.apiKey} key='science' pageSize={this.pageSize} country={this.country} category={'science'}/></Route>
+          <Route exact path="/sports"><News setProgress={this.setProgress} apiKey={this.apiKey} key='sports' pageSize={this.pageSize} country={this.country} category={'sports'}/></Route>
+          <Route exact path="/technology"><News setProgress={this.setProgress} apiKey={this.apiKey} key='technology' pageSize={this.pageSize} country={this.country} category={'technology'}/></Route>
           {/* <Route exact path="/search"><News key='search' pageSize={this.pageSize} country={this.country} category={'anime'}/></Route> */}
         </Switch>
             {/* to change the endpoints we used exact but after changing endpoints the page
